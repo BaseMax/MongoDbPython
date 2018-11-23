337a385d7886128beeb8e3cae423d4f5599f648c
Max Base <MaxBaseCode@Gmail.Com>
2018-11-23 19:48:43 +0330

    Create connect.py

---
+++ connect.py
@@ -0,0 +1,9 @@
+//Some useful libraries
+import pymongo
+import json
+import os
+import sys
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+#dblist = myclient.list_database_names()
+db_name = "name"
+db = client[db_name]
